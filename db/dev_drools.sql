f390a48c9464ea5eee8ae4992c3f477fb9a4acf5
liyi1990129 <545627009@qq.com>
2020-12-21 08:54:31 +0800

    1

--- db/dev_drools.sql
+++ db/dev_drools.sql
@@ -1,17 +1,4 @@
-/*
-Navicat MySQL Data Transfer
 
-Source Server         : localhost
-Source Server Version : 50717
-Source Host           : localhost:3306
-Source Database       : dev_drools
-
-Target Server Type    : MYSQL
-Target Server Version : 50717
-File Encoding         : 65001
-
-Date: 2020-10-22 16:53:57
-*/
 
 SET FOREIGN_KEY_CHECKS=0;
 
